98351b62b0f8c05188fd825c2d2bd67b723403be
atemzaytsev <1308-87@mail.ru>
2019-07-01 14:41:09 +0200

    initial commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -29,3 +29,120 @@ function toggleMenu() {
         showMenu = false;
     }
 }
+//map var 
+    const addressString = "Wyszyńskiego 63, 50-001 Wrocław";
+    const myLatlng = {
+        lat: 51.118010,
+        lng: 17.051591
+    };
+
+    //render google map {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 13,
+        center: myLatlng,
+        styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ]
+    });
+
+    //add marker
+    const marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: "Click to see address"
+
+    });
+
+    //info window
+    const infowindow = new google.maps.InfoWindow({
+        content: addressString
+    });
+
+    //show info window
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
+    });
+
+    //resize function 
+    google.maps.event.addDomListener(window, "resize", function () {
+        const center = map.getCenter();
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(center);
+    });
